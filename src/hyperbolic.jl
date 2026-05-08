9315a5ccd0352a464a74ed15210008e2193cac51
Ronny Bergmann <git@ronnybergmann.net>
2026-05-08 18:50:47 +0200

    Computing in the Computetree is the Computeforest.

--- src/hyperbolic.jl
+++ src/hyperbolic.jl
@@ -22,32 +22,51 @@ fig, ax, p = hyperboloidplot(Hyperbolic(2))
     surfacecolor = :white
     "Surface alpha (0 = transparent, 1 = opaque)."
     surfacealpha = 0.5
-    "range for the x-dimension"
+    "range for the x-dimension for the surface"
     xlims = [-3, 3]
-    "range for the y-dimension"
+    "range for the y-dimension for the surface"
     ylims = [-3, 3]
+    "refinement of points from wires to sampling the surface"
+    refinement = 5
     # add the other default plot attributes here as well
     Makie.mixin_generic_plot_attributes()...
 end
 
 function Makie.plot!(p::HyperboloidPlot{<:Tuple{Hyperbolic{Manifolds.TypeParameter{Tuple{2}}}}})
-    # create a new compute edge
-    # with [:M, :wires] as input nodes (these must already exist)
+    # create a new compute edge – since we have to be careful with
+    # maybe not yet initialized values
+    # in: xlims, refinement, wires,  out: xwire xsurface
+    map!(p.attributes, [:xlims, :refinement, :wires], [:xwire, :xsurface]) do xlims, refinement, wires
+        return range(; start = xlims[1], stop = xlims[2], length = wires), range(; start = xlims[1], stop = xlims[2], length = refinement * wires)
+    end
+    # in: ylims, refinement, wires,  out: ywire ysurface
+    map!(p.attributes, [:ylims, :refinement, :wires], [:ywire, :ysurface]) do ylims, refinement, wires
+        return range(; start = ylims[1], stop = ylims[2], length = wires), range(; start = ylims[1], stop = ylims[2], length = refinement * wires)
+    end
+    # in: xwire, ywire, out: zwire
+    map!(p.attributes, [:xwire, :ywire], :zwire) do x, y
+        return [sqrt(1 + X^2 + Y^2) for X in x, Y in y]
+    end
+    # in: xsurface, ysurface, out: zsurface
+    map!(p.attributes, [:xsurface, :ysurface], :zsurface) do x, y
+        return [sqrt(1 + X^2 + Y^2) for X in x, Y in y]
+    end
+    # in: surfacecolor, out: surfacecolormap (constant)
+    map!(p.attributes, :surfacecolor, :surfacecolormap) do sfc
+        return [sfc]
+    end    # with [:M, :wires] as input nodes (these must already exist)
     # with :sphere_mesh as the output node (this will be created)
     # running the computation defined in the do ... end block
     # where :M is M and :wires is mapped to n
-    x = range(; start = p[:xlims][][1], stop = p[:xlims][][2], length = 10 * p[:wires][])
-    y = range(; start = p[:ylims][][1], stop = p[:ylims][][2], length = 10 * p[:wires][])
-    z = [sqrt(1 + X^2 + Y^2) for X in x, Y in y]
     surface!(
-        p, x, y, z;
-        colormap = [p[:surfacecolor][]], alpha = p[:surfacealpha][], transparency = true,
+        p, p.xsurface, p.ysurface, p.zsurface;
+        colormap = p.surfacecolormap, alpha = p.surfacealpha, transparency = true,
     )
     # a wireframe atop
-    x = range(; start = p[:xlims][][1], stop = p[:xlims][][2], length = p[:wires][])
-    y = range(; start = p[:ylims][][1], stop = p[:ylims][][2], length = p[:wires][])
-    z = [sqrt(1 + X^2 + Y^2) for X in x, Y in y]
-    wireframe!(p, x, y, z; color = p[:wirecolor][], linewidth = p[:wirewidth][], transparency = true)
+    wireframe!(
+        p, p.xwire, p.ywire, p.zwire;
+        color = p.wirecolor, linewidth = p.wirewidth, transparency = true
+    )
     return p
 end
 
